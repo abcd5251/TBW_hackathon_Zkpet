404124681b4275e6f0108e68a03f95b8991e560c
MichaelChen <helloworldsmart3@gmail.com>
2023-12-11 13:52:42 +0800

    feat: add pixelArtImage

--- FrenPet/MyPet/MyPetView.swift
+++ FrenPet/MyPet/MyPetView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct MyPetView: View {
+    @State private var moveRight = false
     var body: some View {
         VStack {
             // Pet Main View
             VStack {
                 PetMetricsView()
+                
+                Image("pixelArtImage")
+                           .resizable()
+                           .scaledToFit()
+                           .frame(width: 200, height: 200)
+                           .background(Color.clear)
+                           .offset(x: moveRight ? 50 : -50, y: 0) // 左右移動 100 點的距離
+                                       .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: moveRight)
+                                       .onAppear() {
+                                           moveRight.toggle() // 當視圖出現時開始動畫
+                                       }
 
                 PetMetricsBottomView()
             }
+            .background(Color(red: 1.0, green: 0.8, blue: 0.6).opacity(0.3))
         }
     }
 }
