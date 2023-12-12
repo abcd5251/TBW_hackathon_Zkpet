8ba976e282999757bfbcec3ea63975a976590bae
MichaelChen <helloworldsmart3@gmail.com>
2023-12-12 11:35:57 +0800

    feat: PetInteractionView

--- FrenPet/MyPet/MyPetView.swift
+++ FrenPet/MyPet/MyPetView.swift
@@ -14,19 +14,21 @@ struct MyPetView: View {
             // Pet Main View
             VStack {
                 PetMetricsView()
-                
+
                 Image("pixelArtImage")
-                           .resizable()
-                           .scaledToFit()
-                           .frame(width: 200, height: 200)
-                           .background(Color.clear)
-                           .offset(x: moveRight ? 50 : -50, y: 0) // 左右移動 100 點的距離
-                                       .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: moveRight)
-                                       .onAppear() {
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .background(Color.clear)
+                    .offset(x: self.moveRight ? 50 : -50, y: 0) // 左右移動 100 點的距離
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: self.moveRight)
+                    .onAppear {
 //                                           moveRight.toggle() // 當視圖出現時開始動畫
-                                       }
+                    }
 
                 PetMetricsBottomView()
+
+                PetInteractionView()
             }
             .background(Color(red: 1.0, green: 0.8, blue: 0.6).opacity(0.3))
         }
---
+++ FrenPet/MyPet/PetInteractionView.swift
@@ -0,0 +1,75 @@
+//
+//  PetInteractionView.swift
+//  FrenPet
+//
+//  Created by Michael on 2023/12/12.
+//
+
+import SwiftUI
+
+struct PetInteractionView: View {
+    var body: some View {
+        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
+            Button(action: {
+                print("fish button tapped")
+            }) {
+                Image(systemName: "fish")
+                    .buttonStyle()
+            }
+
+            Button(action: {
+                print("bird button tapped")
+            }) {
+                Image(systemName: "bird")
+                    .buttonStyle()
+            }
+
+            Button(action: {
+                print("carrot button tapped")
+            }) {
+                Image(systemName: "carrot")
+                    .buttonStyle()
+            }
+
+            Button(action: {
+                print("leaf button tapped")
+            }) {
+                Image(systemName: "leaf")
+                    .buttonStyle()
+            }
+
+            Button(action: {
+                print("waterbottle button tapped")
+            }) {
+                Image(systemName: "waterbottle")
+                    .buttonStyle()
+            }
+
+            Button(action: {
+                print("birthday.cake button tapped")
+            }) {
+                Image(systemName: "birthday.cake")
+                    .buttonStyle()
+            }
+        }
+        .padding()
+    }
+}
+
+// 定義一個按鈕樣式的擴展，用於所有按鈕以保持一致的外觀
+extension Image {
+    func buttonStyle() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50)
+            .padding()
+            .background(Color(red: 1.0, green: 0.8, blue: 0.6).opacity(0.3))
+            .foregroundColor(Color(red: 1.0, green: 0.8, blue: 0.6).opacity(0.9))
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    PetInteractionView()
+}
